a6c52db3fcdd154f7c849dcf5bb5b1dc6d913c6c
yana <africa_07@mail.ru>
2015-04-21 18:00:44 +0300

    1

---
+++ src/smoke/cFirst.java
@@ -0,0 +1,13 @@
+package smoke;
+
+/**
+ * Created by yana on 21.04.15.
+ */
+public class cFirst {
+
+    public static void main (String[] arg) {
+
+        System.out.println("hi");
+        System.out.println("hi gihdfjgdjsfgj");
+    }
+}
