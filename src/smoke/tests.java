ef13325346b0d8b48c7d132a935bfbc32b950a5c
yana <africa_07@mail.ru>
2015-04-22 15:45:46 +0300

    3

--- src/smoke/tests.java
+++ src/smoke/tests.java
@@ -22,17 +22,14 @@ public class tests {
         Assert.assertEquals("Fail", expRes, expRes);
     }
 
+
+
     @Before
     public void start()
     {   System.setProperty("webdriver.chrome.driver", "/home/yana/QAFactory/chromedriver");
         //System.setProperty("webdriver.chrome.driver", "C:/Automation/chromedriver.exe");
         drv = new ChromeDriver();
-        //drv.manage().window().maximize();
-        //drv.get("http://angel.net/~nic/passwd.current.html");
-        //drv.get("http://oxogamestudio.com/passwd.current2.htm");
-        //drv.get("http://oxogamestudio.com/passwd.current3.htm");
         drv.get("http://newshubtest.org");
-
     }
 
     @After
